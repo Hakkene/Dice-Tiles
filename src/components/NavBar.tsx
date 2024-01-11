fc5eef116b8f21f93c89a4d6bc4e50aafcae21ac
Marcel Okonowski <marcelokonowski@gmail.com>
2024-01-11 19:00:03 +0100

    updated gamelist and titlescreen

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,12 +3,11 @@ const NavBar = () => {
     <>
       <div className="w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
         <div>
-          <h1 className="text-4xl font-bold"><a href="/">Dice&Tiles</a></h1>
+          <h1 className="text-4xl font-bold">
+            <a href="/">Dice&Tiles</a>
+          </h1>
         </div>
         <ul className=" gap-x-4 flex flex-row">
-          <li>
-            <a href="/title">Games - titlescreen for now</a>
-          </li>
           <li>
             <a href="/gamelist">GamesList</a>
           </li>
--- src/screens/TitleScreen.tsx
+++ src/screens/TitleScreen.tsx
@@ -3,8 +3,11 @@ import Description from "../components/TitleComponents/Description";
 import ImageGallery from "../components/TitleComponents/ImageGallery";
 import Recomendations from "../components/TitleComponents/Recomendations";
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const TitleScreen = () => {
+  const { gameName } = useParams<{ gameName: string }>();
+
   const sampleComments = [
     {
       userIcon: "url-to-user-icon1.jpg",
@@ -20,17 +23,6 @@ const TitleScreen = () => {
     },
   ];
 
-  const images = [
-    "/assets/mr2_AI_purple.jpg",
-    "/assets/mr2_AI_red.png",
-    "/assets/ionia.jpg",
-    "/assets/mr2_AI_purple.jpg",
-    "/assets/mr2_AI_red.png",
-    "/assets/ionia.jpg",
-    "/assets/mr2_AI_purple.jpg",
-    "/assets/mr2_AI_red.png",
-  ];
-
   const producer = "Producent";
   const rating = 98;
 
@@ -39,11 +31,9 @@ const TitleScreen = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(
-          "http://localhost:8000/fetchedproducts/?name=Pandemic Legacy: Season 1"
-        );
+        const response = await fetch("http://localhost:3001/api/products/");
         const data = await response.json();
-        setFetchedProducts(data);
+        setFetchedProducts(data.results || []);
         console.log("Dane z API:", data);
       } catch (error) {
         console.error("Błąd pobierania danych:", error);
@@ -53,40 +43,45 @@ const TitleScreen = () => {
     fetchData();
   }, []);
 
-  const handleRecommendatiClick = (item: string) => {
+  const selectedProduct = fetchedProducts.find(
+    (product) => product.name === gameName
+  );
+
+  if (!selectedProduct) {
+    return <div>Loading...</div>; // You might want to handle the case where the product is not found
+  }
+
+  const handleRecommendationClick = (item: string) => {
     console.log(item);
   };
+
+  const images = [
+    selectedProduct.image1,
+    selectedProduct.image2,
+    selectedProduct.image3,
+    selectedProduct.image4,
+    selectedProduct.image5,
+  ];
+
   return (
-    <>
-      <div className="w-3/5 mx-auto flex flex-col justify-center">
-        <div>
-          {fetchedProducts.map((product) => (
-            <ImageGallery
-              title={product.name}
-              image_url={product.image_url}
-              description={product.description}
-              images={images}
-              producer={producer}
-              rating={rating}
-            />
-          ))}
-        </div>
-        <div>
-          {fetchedProducts.map((product) => (
-            <Description description={product.description} />
-          ))}
-        </div>
-        <div>
-          <Recomendations
-            recommendations={images}
-            onRecommendationClick={handleRecommendatiClick}
-          />
-        </div>
-        <div>
-          <CommentSection comments={sampleComments} />
-        </div>
-      </div>
-    </>
+    <div className="w-3/5 mx-auto flex flex-col justify-center">
+      <ImageGallery
+        title={selectedProduct.name}
+        image_url={selectedProduct.image1}
+        description={selectedProduct.description}
+        images={images}
+        upvotes={selectedProduct.upvotes}
+        downvotes={selectedProduct.downvotes}
+        min_players={selectedProduct.min_players}
+        max_players={selectedProduct.max_players}
+      />
+      <Description description={selectedProduct.description} />
+      <Recomendations
+        recommendations={images}
+        onRecommendationClick={handleRecommendationClick}
+      />
+      <CommentSection comments={sampleComments} />
+    </div>
   );
 };
 
