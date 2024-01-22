621150cece08961c3d049aa72e7e21a8a3f3dca8
Marcel Okonowski <marcelokonowski@gmail.com>
2024-01-22 02:50:18 +0100

    minify

--- src/components/TitleComponents/Description.tsx
+++ src/components/TitleComponents/Description.tsx
@@ -1,14 +1,1 @@
-interface Props {
-  description: string;
-}
-
-const Description = ({ description }: Props) => {
-  return (
-    <div className="mb-5">
-      <h3 className="m-4 text-2xl font-bold">Description</h3>
-      <p className="description-text">{description}</p>
-    </div>
-  );
-};
-
-export default Description;
+interface Props{description:string}const Description=({description}:Props)=>{return(<div className="mb-5"><h3 className="m-4 text-2xl font-bold">Description</h3><p className="description-text">{description}</p></div>)};export default Description
